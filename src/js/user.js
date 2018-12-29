2c045bde5d57990ebc4edb42a5f884bdbfccd720
tomek <tomek371240@gmail.com>
2018-12-29 21:04:03 +0100

    first-commit

---
+++ src/js/user.js
@@ -0,0 +1,1 @@
+export const name = `userName: Thomas Thomson`;
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
+module.exports = {
+    entry: {
+        app: './src/js/app.js',
+        users: './src/js/users.js',
+        login: './src/js/login.js'
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].entry.js"
+    },
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            },
+            {
+                test:/\.css$/,
+                use: ['style-loader','css-loader']
+            }
+
+            // {
+            //     test: /\.html$/,
+            //     use: [
+            //         {
+            //             loader: 'file-loader',
+            //             options: {
+            //                 name: '[name].[ext]'
+            //             }
+            //         }
+            //     ],
+            //     exclude: path.resolve(__dirname, 'src/index.html')
+            // }
+        ]
+    },
+    plugins: [
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'users.html',
+            template: 'src/users.html',
+            chunks: ['users']
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+};
